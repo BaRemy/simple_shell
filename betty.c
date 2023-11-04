2b6fb5227da90763e79047a74b60ee7a76a25783
mugabe-rob <mugaberobert222004644@gmail.com>
2023-11-04 10:09:03 -0700

    A programm to make betty happy

---
+++ betty.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description: A program to display hello world
+ * Return: 0 Always
+ */
+int main(void)
+{
+	printf("Hello world\n");
+	return (0);
+}
