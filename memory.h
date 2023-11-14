546bd90e2867a40c18d1520cf5045330ec52fdb9
BaRemy <bahatiremy05@gmail.com>
2023-11-13 16:14:07 -0800

    Define memory management functions

---
+++ memory.h
@@ -0,0 +1,13 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include <stdlib.h>
+
+/* memory */
+void *_realloc(void *ptr, size_t old_size, size_t new_size);
+
+/* free.c */
+void free_memory_p(void *ptr);
+void free_memory_pp(void **ptr);
+
+#endif /* MEMORY_H */
