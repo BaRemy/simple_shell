d7827f1f549da3db09df07b407873dc7744df44d
BaRemy <bahatiremy05@gmail.com>
2023-11-14 04:53:53 -0800

    Add file descriptor output functions

---
+++ printers.c
@@ -0,0 +1,36 @@
+#include "general.h"
+#include "text.h"
+#include "unistd.h"
+
+/**
+ * _putchar_to_fd - Print a character to specific file
+ * decriptor
+ *
+ * @c: Character to print
+ * @fd: Place to print the character
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar_to_fd(char c, int fd)
+{
+	return (write(fd, &c, 1));
+}
+
+/**
+ * print_to_fd - Print a string
+ *
+ * @msg: Strin to print in the fd specified
+ * @fd: File descripot to print
+ *
+ * Return: On succes numbers of bytes printed.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int print_to_fd(char *msg, int fd)
+{
+	int size;
+
+	size = _strlen(msg);
+
+	return (write(fd, msg, size));
+}
